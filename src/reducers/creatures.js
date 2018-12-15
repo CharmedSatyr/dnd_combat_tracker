c4414edb1908835cd08dfd63982901a5671758ad
CharmedSatyr <z@charmed.tech>
2018-12-14 20:25:01 -0600

    refactor, remove sorting in favor of display side sorting

--- src/reducers/creatures.js
+++ src/reducers/creatures.js
@@ -12,17 +12,14 @@ export default (state = [], payload) => {
     case c.ROLL_INITIATIVE:
       const roll = creatures => {
         const d20 = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min
-        const initiativeOrder = creatures
-          .map(c => {
-            const min = 1 + parseInt(c.modifier)
-            const max = 20 + parseInt(c.modifier)
-            const roll1 = d20(min, max)
-            const roll2 = d20(min, max)
-            const higher = roll1 >= roll2 ? roll1 : roll2
-            return { ...c, initiative: c.advantage ? higher : roll1 }
-          })
-          .sort((a, b) => b.initiative - a.initiative)
-        return initiativeOrder
+        return creatures.map(c => {
+          const min = 1 + parseInt(c.modifier)
+          const max = 20 + parseInt(c.modifier)
+          const roll1 = d20(min, max)
+          const roll2 = d20(min, max)
+          const higher = roll1 >= roll2 ? roll1 : roll2
+          return { ...c, initiative: c.advantage ? higher : roll1 }
+        })
       }
       updated = [...roll(state)]
       return updated
