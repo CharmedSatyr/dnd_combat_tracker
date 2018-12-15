0818338de48418e0428b30662d677cdc8fcfcf1b
CharmedSatyr <z@charmed.tech>
2018-12-15 14:00:48 -0600

    +page header; use grid for alignment; bg color

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,24 @@
 import React, { Component } from 'react'
 import Initiative from './Initiative'
 
+import { Grid, PageHeader, Row } from 'react-bootstrap'
+
 class App extends Component {
   render() {
     return (
       <div
         style={{
-          background: 'lightgray',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
+          backgroundColor: '#f9f9f9',
           width: '100%',
           height: '100vh',
         }}
       >
-        <Initiative />
+        <Grid>
+          <PageHeader>D&amp;D Initiative Tracker</PageHeader>
+          <Row className="show-grid">
+            <Initiative />
+          </Row>
+        </Grid>
       </div>
     )
   }
