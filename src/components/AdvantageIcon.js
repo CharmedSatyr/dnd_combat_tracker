5abef3f2553538dbb13a16e6bf90cf2172d9e5b8
CharmedSatyr <z@charmed.tech>
2018-12-14 20:22:08 -0600

    consolidated Icons; specialized InitiativeForm for Players

--- src/components/AdvantageIcon.js
+++ src/components/AdvantageIcon.js
@@ -1,13 +1,1 @@
 import React from 'react'
-
-import { Glyphicon } from 'react-bootstrap'
-
-const AdvantageIcon = () => (
-  <span>
-    &nbsp;
-    <Glyphicon title="Rolls with advantage" glyph="text-background" style={{ color: '#5cb85c' }} />
-    &nbsp;
-  </span>
-)
-
-export default AdvantageIcon
---
+++ src/components/Icons.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react'
+import * as c from '../constants'
+
+import { Glyphicon } from 'react-bootstrap'
+
+export const AdvantageIcon = () => (
+  <span>
+    &nbsp;
+    <Glyphicon title="Advantage" glyph="text-background" style={{ color: '#5cb85c' }} />
+    &nbsp;
+  </span>
+)
+
+export const HourglassIcon = () => <Glyphicon glyph="hourglass" style={{ color: '#555' }} />
+
+export const PlayerIcon = () => (
+  <span>
+    &nbsp;
+    <Glyphicon glyph="user" style={{ color: '#555' }} />
+    &nbsp;
+  </span>
+)
+
+export class RemoveIcon extends Component {
+  constructor(props) {
+    super(props)
+    this.removeCreature = this.removeCreature.bind(this)
+  }
+  removeLocal(id) {
+    let creatures
+    if (localStorage.hasOwnProperty(c.LOCAL_CREATURES)) {
+      creatures = localStorage.getItem(c.LOCAL_CREATURES)
+      try {
+        creatures = JSON.parse(creatures)
+        creatures = creatures.filter(c => c.id !== this.props.id)
+        creatures = JSON.stringify(creatures)
+        localStorage.setItem(c.LOCAL_CREATURES, creatures)
+      } catch (e) {
+        console.error('Error:', e)
+      }
+    }
+  }
+  removeCreature() {
+    this.props.removeCreature(this.props.id)
+    this.removeLocal(this.props.id)
+  }
+  render() {
+    return (
+      <span>
+        <Glyphicon
+          glyph="remove"
+          onClick={this.removeCreature}
+          style={{ color: '#a94442' }}
+          title="Remove creature"
+        />
+        &nbsp;
+      </span>
+    )
+  }
+}
