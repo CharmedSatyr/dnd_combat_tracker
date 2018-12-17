9d4f6ff3946a2518b3dbd361f88e7fc69ef20c2e
CharmedSatyr <z@charmed.tech>
2018-12-17 11:33:27 -0600

    roll by group

--- src/reducers/creatures.js
+++ src/reducers/creatures.js
@@ -12,18 +12,49 @@ export default (state = [], payload) => {
       updated = [...state.filter(creature => creature.id !== payload.id)]
       return updated
     case c.ROLL_INITIATIVE:
-      const roll = creatures => {
-        const d20 = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min
-        return creatures.map(c => {
-          const min = 1 + parseInt(c.modifier)
-          const max = 20 + parseInt(c.modifier)
-          const roll1 = d20(min, max)
-          const roll2 = d20(min, max)
-          const higher = roll1 >= roll2 ? roll1 : roll2
-          return { ...c, initiative: c.advantage ? higher : roll1 }
-        })
+      const d20 = modifier => {
+        const min = 1 + parseInt(modifier)
+        const max = 20 + parseInt(modifier)
+        return Math.floor(Math.random() * (max - (min + 1)) + min)
       }
-      updated = [...roll(state)]
+
+      const d20A = modifier => {
+        const roll1 = d20(modifier)
+        const roll2 = d20(modifier)
+        return roll1 >= roll2 ? roll1 : roll2
+      }
+
+      const roll = state => {
+        let creatures = [...state]
+        const tags = {}
+        // Loop through creatures once
+        creatures.forEach(cr => {
+          // If it has a group
+          if (cr.tag) {
+            // check tags obj for group's initiative
+            if (tags.hasOwnProperty(cr.tag) && tags[cr.tag]) {
+              cr.initiative = tags[cr.tag]
+              // otherwise roll it!
+              // with advantage
+            } else if (cr.advantage) {
+              tags[cr.tag] = d20A(cr.modifier)
+              cr.initiative = tags[cr.tag]
+              // or without advantage
+            } else {
+              tags[cr.tag] = d20(cr.modifier)
+              cr.initiative = tags[cr.tag]
+            }
+            // If no group, if it has advantage, roll w/
+          } else if (cr.advantage) {
+            cr.initiative = d20A(cr.modifier)
+            // if it doesn't, don't
+          } else {
+            cr.initiative = d20(cr.modifier)
+          }
+        })
+        return creatures
+      }
+      updated = roll(state)
       return updated
     case c.SET_STATE_FROM_LOCAL:
       return [...payload.localCreatures]
