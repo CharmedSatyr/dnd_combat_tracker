999c971765c0442ab3ad51d78a42b606ea4285c8
CharmedSatyr <z@charmed.tech>
2018-12-18 16:13:17 -0600

    do all sorting here, inc. by initiative order

--- src/reducers/creatures.js
+++ src/reducers/creatures.js
@@ -44,16 +44,40 @@ export default (state = [], payload) => {
               groupIDs[cr.groupID] = d20(cr.modifier)
               cr.initiative = groupIDs[cr.groupID]
             }
-            // If no group, if it has advantage, roll w/
-          } else if (cr.advantage) {
-            cr.initiative = d20A(cr.modifier)
-            // if it doesn't, don't
-          } else {
-            cr.initiative = d20(cr.modifier)
+            // If no group, use ID
+          } else if (cr.id) {
+            const shortID = cr.id.split('-')[1]
+            // If advantage, roll w/
+            if (cr.advantage) {
+              groupIDs[shortID] = d20A(cr.modifier)
+              cr.initiative = groupIDs[cr.shortID]
+              // if it doesn't, don't
+            } else {
+              groupIDs[shortID] = d20(cr.modifier)
+              cr.initiative = groupIDs[shortID]
+            }
           }
         })
+
+        // Get an array of all the Group or Player IDs in initiative order
+        const initiativeOrder = []
+          .concat(...Object.entries(groupIDs).sort((a, b) => b[1] - a[1]))
+          .filter(o => typeof o === 'string')
+
+        // Give each creature object an order property based on that
+        creatures.forEach(
+          cr =>
+            (cr.order = initiativeOrder.indexOf(cr.groupID ? cr.groupID : cr.id.split('-')[1]) + 1)
+        )
+        // Sort creatures
         return creatures
+          .sort((a, b) => (a.name > b.name ? -1 : 1)) // by name
+          .sort((a, b) => a.number && b.number && a.number - b.number) // by tag number
+          .sort((a, b) => (b.advantage ? 1 : -1)) // by advantage
+          .sort((a, b) => b.modifier - a.modifier) // by modifier
+          .sort((a, b) => a.order - b.order) // by order
       }
+
       updated = roll(state)
       return updated
     case c.SET_STATE_FROM_LOCAL:
