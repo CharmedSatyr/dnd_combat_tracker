51ebea07bb8956d589f3e8011ab6687fe35e210a
CharmedSatyr <z@charmed.tech>
2018-12-20 12:48:06 -0600

    use static ids for default players

--- src/constants/witchers.js
+++ src/constants/witchers.js
@@ -1,40 +1,38 @@
-import { setID } from '../components/component.functions'
-
 const brunhild = {
   name: 'Brunhild',
   modifier: 4,
   advantage: false,
-  id: `player-${setID()}`,
+  id: 'player-100',
 }
 const hishiro = {
   name: 'Hishiro Gozen',
   modifier: 1,
   advantage: false,
-  id: `player-${setID()}`,
+  id: 'player-200',
 }
 const keph = {
   name: 'Keph Thrassden',
   modifier: 6,
   advantage: true,
-  id: `player-${setID()}`,
+  id: 'player-300',
 }
 const marni = {
   name: 'Marni Moonfoot',
   modifier: 5,
   advantage: false,
-  id: `player-${setID()}`,
+  id: 'player-400',
 }
 const rokas = {
   name: 'Rokas Rothenel',
   modifier: 5,
   advantage: true,
-  id: `player-${setID()}`,
+  id: 'player-500',
 }
 const shadow = {
   name: 'Shadow',
   modifier: 9,
   advantage: false,
-  id: `player-${setID()}`,
+  id: 'player-600',
 }
 
 export default [marni, brunhild, keph, shadow, rokas, hishiro]
