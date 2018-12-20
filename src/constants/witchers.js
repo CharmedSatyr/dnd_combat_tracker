0c4599658e11ef5e1115958bdd0769830cd5158a
CharmedSatyr <z@charmed.tech>
2018-12-20 12:34:54 -0600

    current monster hunting 5e players

---
+++ src/constants/witchers.js
@@ -0,0 +1,40 @@
+import { setID } from '../components/AddCreature/addCreature.functions'
+
+const brunhild = {
+  name: 'Brunhild',
+  modifier: 4,
+  advantage: false,
+  id: `player-${setID()}`,
+}
+const hishiro = {
+  name: 'Hishiro Gozen',
+  modifier: 1,
+  advantage: false,
+  id: `player-${setID()}`,
+}
+const keph = {
+  name: 'Keph Thrassden',
+  modifier: 6,
+  advantage: true,
+  id: `player-${setID()}`,
+}
+const marni = {
+  name: 'Marni Moonfoot',
+  modifier: 5,
+  advantage: false,
+  id: `player-${setID()}`,
+}
+const rokas = {
+  name: 'Rokas Rothenel',
+  modifier: 5,
+  advantage: true,
+  id: `player-${setID()}`,
+}
+const shadow = {
+  name: 'Shadow',
+  modifier: 9,
+  advantage: false,
+  id: `player-${setID()}`,
+}
+
+export default [marni, brunhild, keph, shadow, rokas, hishiro]
