38c3818200e86cc0828f8a5a1836cffd1f562eb0
CharmedSatyr <z@charmed.tech>
2018-12-27 16:21:43 -0600

    use experience reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
 import creatures from './creatures'
+import experience from './experience'
 import { combineReducers } from 'redux'
 
-const rootReducer = combineReducers({ creatures })
+const rootReducer = combineReducers({ creatures, experience })
 
 export default rootReducer
