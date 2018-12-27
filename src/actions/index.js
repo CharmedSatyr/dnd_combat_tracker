ae403c2a679c7480cdba2a8b9ab3532467f55feb
CharmedSatyr <z@charmed.tech>
2018-12-26 22:32:43 -0600

    player count actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,6 +5,10 @@ export const addCreatures = creatures => ({ type: c.ADD_CREATURES, creatures })
 export const decrementCreatureInitiative = id => ({ type: c.DECREMENT_CREATURE_INITIATIVE, id })
 export const incrementCreatureInitiative = id => ({ type: c.INCREMENT_CREATURE_INITIATIVE, id })
 
+// Player Count
+export const incrementPlayerCount = number => ({ type: c.INCREMENT_PLAYER_COUNT, number })
+export const decrementPlayerCount = number => ({ type: c.DECREMENT_PLAYER_COUNT, number })
+
 export const removeCreature = id => ({ type: c.REMOVE_CREATURE, id })
 
 export const rollInitiative = () => ({ type: c.ROLL_INITIATIVE })
