048cb9255dfedbbf3dacdd632a5f0008a65c46f6
CharmedSatyr <z@charmed.tech>
2018-12-28 10:49:57 -0600

    update experience when creature removed or set locally

--- src/reducers/experience.js
+++ src/reducers/experience.js
@@ -8,21 +8,36 @@ const defaultState = {
 }
 
 export default (state = defaultState, payload) => {
+  let playerCount, totalXP, xpPerPlayer
   switch (payload.type) {
     case c.ADD_CREATURES:
-      const playerCount =
+      playerCount =
         payload.creatures.filter(c => c.id.split('-')[0] === 'player').length + state.playerCount
-      const totalXP =
+      totalXP =
         payload.creatures
           .filter(c => c.id.split('-')[0] === 'monster')
           .reduce((acc, curr) => acc + curr.xp, 0) + state.totalXP
-      const xpPerPlayer = playerCount > 0 ? totalXP / playerCount : 0
+      xpPerPlayer = playerCount > 0 ? totalXP / playerCount : 0
 
       return Object.assign({}, state, { playerCount, totalXP, xpPerPlayer })
     case c.REMOVE_CREATURE:
-      return state
+      const type = payload.creature.id.split('-')[0]
+      playerCount = type === 'player' ? state.playerCount - 1 : state.playerCount
+      totalXP = type === 'monster' ? state.totalXP - payload.creature.xp : state.totalXP
+      xpPerPlayer = playerCount > 0 ? totalXP / playerCount : 0
+
+      return Object.assign({}, state, { playerCount, totalXP, xpPerPlayer })
     case c.SET_STATE_FROM_LOCAL:
-      return state
+      playerCount =
+        payload.localCreatures.filter(c => c.id.split('-')[0] === 'player').length +
+        state.playerCount
+      totalXP =
+        payload.localCreatures
+          .filter(c => c.id.split('-')[0] === 'monster')
+          .reduce((acc, curr) => acc + curr.xp, 0) + state.totalXP
+      xpPerPlayer = playerCount > 0 ? totalXP / playerCount : 0
+
+      return Object.assign({}, state, { playerCount, totalXP, xpPerPlayer })
     default:
       return state
   }
