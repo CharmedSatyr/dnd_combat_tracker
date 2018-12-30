f7d25e39d76c9ecc52d20b1b3aade5df7e007a05
CharmedSatyr <z@charmed.tech>
2018-12-29 19:54:36 -0600

    only show Experience if there are creatures.

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -4,18 +4,23 @@ import PropTypes from 'prop-types'
 
 import { Col } from 'react-bootstrap'
 
-const Experience = ({ playerCount, totalXP }) => (
-  <Col xs={12} md={4} className="well">
-    Players: {parseInt(playerCount)}
-    <br />
-    Total Experience: {parseInt(totalXP)}
-    <br />
-    Experience/Player:{' '}
-    {parseInt(playerCount) > 0 ? Math.floor(parseInt(totalXP) / parseInt(playerCount)) : 0}
-  </Col>
+const Experience = ({ creatures, playerCount, totalXP }) => (
+  <span>
+    {creatures.length > 0 && (
+      <Col xs={12} md={4} className="well">
+        Players: {parseInt(playerCount)}
+        <br />
+        Total Experience: {parseInt(totalXP)}
+        <br />
+        Experience/Player:{' '}
+        {parseInt(playerCount) > 0 ? Math.floor(parseInt(totalXP) / parseInt(playerCount)) : 0}
+      </Col>
+    )}
+  </span>
 )
 
 const mapStateToProps = (state, props) => ({
+  creatures: state.creatures,
   playerCount: state.experience.playerCount,
   totalXP: state.experience.totalXP,
   xpPerPlayer: state.experience.xpPerPlayer,
@@ -27,6 +32,7 @@ export default connect(
 )(Experience)
 
 Experience.propTypes = {
+  creatures: PropTypes.arrayOf(PropTypes.object),
   playerCount: PropTypes.number.isRequired,
   totalXP: PropTypes.number.isRequired,
   xpPerPlayer: PropTypes.number.isRequired,
