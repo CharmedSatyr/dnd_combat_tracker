8bc95a1d46d61be3e3b51c7bf6f021a8e25c2391
CharmedSatyr <z@charmed.tech>
2018-12-31 11:34:36 -0600

    parseInt for all number values before storing

--- src/constants/creature.js
+++ src/constants/creature.js
@@ -1,13 +1,13 @@
 class Creature {
   constructor(name, modifier, advantage, id, ac, hp, xp, tag, number, groupID) {
     this.name = name
-    this.modifier = modifier
+    this.modifier = parseInt(modifier)
     this.advantage = advantage
-    this.ac = ac
-    this.hp = hp
-    this.xp = xp
+    this.ac = parseInt(ac)
+    this.hp = parseInt(hp)
+    this.xp = parseInt(xp)
     this.tag = tag
-    this.number = number
+    this.number = parseInt(number)
     this.id = id
     this.groupID = groupID
   }
