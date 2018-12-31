4238bfb566524c03d8a091b2fc2b6a909983d4d8
CharmedSatyr <z@charmed.tech>
2018-12-31 11:45:00 -0600

    move Experience component to folder

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Initiative from './Initiative/'
 import AddCreature from './AddCreature/'
-import Experience from './Experience'
+import Experience from './Experience/'
 
 import { Grid, PageHeader, Row } from 'react-bootstrap'
 
