93d294c66dfa0766c815a80a47e13c59e26509c3
CharmedSatyr <z@charmed.tech>
2019-01-02 15:26:37 -0600

    move rollInitiative function to external file

--- src/reducers/creatures.js
+++ src/reducers/creatures.js
@@ -53,56 +53,7 @@ export default (state = [], payload) => {
       updated = [...state.filter(creature => creature.id !== payload.creature.id)]
       return updated
     case c.ROLL_INITIATIVE:
-      const roll = creaturesArray => {
-        let creatures = [...creaturesArray]
-        const groupIDs = {}
-        // Loop through creatures once
-        creatures.forEach(cr => {
-          // If it has a group
-          if (cr.groupID) {
-            // check groupIDs obj for group's initiative
-            if (groupIDs.hasOwnProperty(cr.groupID) && groupIDs[cr.groupID]) {
-              cr.initiative = groupIDs[cr.groupID]
-              // otherwise roll for initiative!
-              // with advantage
-            } else if (cr.advantage) {
-              groupIDs[cr.groupID] = f.d20A(cr.modifier)
-              cr.initiative = groupIDs[cr.groupID]
-              // or without advantage
-            } else {
-              groupIDs[cr.groupID] = f.d20(cr.modifier)
-              cr.initiative = groupIDs[cr.groupID]
-            }
-            // If the creature has no group, use its id
-          } else if (cr.id) {
-            // only the number part
-            const shortID = cr.id.split('-')[1]
-            // If it has advantage, roll w/
-            if (cr.advantage) {
-              groupIDs[shortID] = f.d20A(cr.modifier)
-              cr.initiative = groupIDs[shortID]
-              // if it doesn't, don't
-            } else {
-              groupIDs[shortID] = f.d20(cr.modifier)
-              cr.initiative = groupIDs[shortID]
-            }
-          }
-        })
-
-        // Get an array of all the Group or Player IDs in initiative order
-        const initiativeOrder = []
-          .concat(...Object.entries(groupIDs).sort((a, b) => b[1] - a[1]))
-          .filter(o => typeof o === 'string')
-
-        // Give each creature object an order property based on that
-        creatures.forEach(
-          cr =>
-            (cr.order = initiativeOrder.indexOf(cr.groupID ? cr.groupID : cr.id.split('-')[1]) + 1)
-        )
-        // Sort creatures
-        return f.sortCreaturesArray(creatures)
-      }
-      updated = roll(state)
+      updated = f.rollInitiative(state)
       return updated
     default:
       return state
