4c70e908dac4293d8f4b3993296136e862285128
CharmedSatyr <z@charmed.tech>
2019-01-05 23:28:08 -0600

    +conditions list

--- src/components/Conditions/index.js
+++ src/components/Conditions/index.js
@@ -4,3 +4,20 @@ import { connect } from 'react-redux'
 const Conditions = () => <div>placeholder</div>
 
 export default Conditions
+
+/* Blinded
+ * Charmed
+ * Deafened
+ * Frightened
+ * Grappled
+ * Incapacitated
+ * Invisible
+ * Paralyzed
+ * Petrified
+ * Poisoned
+ * Prone
+ * Restrained
+ * Stunned
+ * Unconscious
+ * Exhaustion
+ */
