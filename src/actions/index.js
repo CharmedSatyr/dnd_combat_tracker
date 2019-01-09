cf5c429b1724c2cd65994f0ddb6011cc7203152d
CharmedSatyr <z@charmed.tech>
2019-01-09 13:52:51 -0600

    updating to incrementGroupInitiativeOrder func name

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,11 @@ import * as c from '../constants/index'
 export const addCreatures = creatures => ({ type: c.ADD_CREATURES_TO_STATE, creatures })
 
 export const decrementCreatureInitiative = id => ({ type: c.DECREMENT_CREATURE_INITIATIVE, id })
-export const incrementCreatureInitiative = id => ({ type: c.INCREMENT_CREATURE_INITIATIVE, id })
+
+export const incrementGroupInitiativeOrder = groupID => ({
+  type: c.INCREMENT_GROUP_INITIATIVE_ORDER,
+  groupID,
+})
 
 export const removeCreature = creature => ({ type: c.REMOVE_CREATURE_FROM_STATE, creature })
 
