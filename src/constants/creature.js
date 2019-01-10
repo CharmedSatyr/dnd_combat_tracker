92bb3ebd8e639c2ed7c0b9806c6dd696a62e619a
CharmedSatyr <z@charmed.tech>
2019-01-10 13:15:53 -0600

    add lair and legendary properties

--- src/constants/creature.js
+++ src/constants/creature.js
@@ -1,11 +1,13 @@
 class Creature {
-  constructor(name, modifier, advantage, id, ac, hp, xp, tag, number, groupID) {
+  constructor(name, modifier, advantage, id, ac, hp, xp, lair, legendary, tag, number, groupID) {
     this.name = name
     this.modifier = parseInt(modifier)
     this.advantage = advantage
     this.ac = parseInt(ac)
     this.hp = parseInt(hp)
     this.xp = parseInt(xp)
+    this.lair = parseInt(lair)
+    this.legendary = parseInt(legendary)
     this.tag = tag
     this.number = parseInt(number)
     this.id = id
