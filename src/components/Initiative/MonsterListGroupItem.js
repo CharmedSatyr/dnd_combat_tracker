c57093adda7626dbbbce4428b5ce1ad8e4e96afa
CharmedSatyr <z@charmed.tech>
2019-01-11 15:08:57 -0600

    styling (no wrapping brackets)

--- src/components/Initiative/MonsterListGroupItem.js
+++ src/components/Initiative/MonsterListGroupItem.js
@@ -8,87 +8,85 @@ import IncrementDecrementButtons from './IncrementDecrementButtons'
 
 import { setLabel, initiativeLength } from '../component.functions'
 
-const MonsterListGroupItem = ({ monster, removeCreature }) => {
-  return (
+const MonsterListGroupItem = ({ monster, removeCreature }) => (
+  <div
+    className="list-group-item"
+    style={{
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+    }}
+  >
+    {/* LEFT */}
     <div
-      className="list-group-item"
       style={{
-        display: 'flex',
-        justifyContent: 'space-between',
         alignItems: 'center',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
       }}
     >
-      {/* LEFT */}
-      <div
-        style={{
-          alignItems: 'center',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-        }}
-      >
-        <Label bsStyle="info">{monster.order}</Label>
-        <RemoveIcon creature={monster} removeCreature={removeCreature} />
-      </div>
-      {/* ORDER CONTROL */}
-      {monster.order && <IncrementDecrementButtons groupID={monster.groupID} />}
-      {/* CENTER */}
-      <div style={{ width: '65%' }}>
-        <MonsterIcon />
-        <strong>{monster.name}</strong>&nbsp;
-        <Label>{setLabel(monster.tag, monster.number)}</Label>
-        <br />
+      <Label bsStyle="info">{monster.order}</Label>
+      <RemoveIcon creature={monster} removeCreature={removeCreature} />
+    </div>
+    {/* ORDER CONTROL */}
+    {monster.order && <IncrementDecrementButtons groupID={monster.groupID} />}
+    {/* CENTER */}
+    <div style={{ width: '65%' }}>
+      <MonsterIcon />
+      <strong>{monster.name}</strong>&nbsp;
+      <Label>{setLabel(monster.tag, monster.number)}</Label>
+      <br />
+      <span>
+        <span style={{ color: '#555' }}>Modifier:&nbsp;</span>
+        <strong>{monster.modifier >= 0 ? `+${monster.modifier}` : `${monster.modifier}`}</strong>
+        {monster.advantage && <AdvantageIcon />}
+      </span>
+      {/* MID CENTER */}
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
         <span>
-          <span style={{ color: '#555' }}>Modifier:&nbsp;</span>
-          <strong>{monster.modifier >= 0 ? `+${monster.modifier}` : `${monster.modifier}`}</strong>
-          {monster.advantage && <AdvantageIcon />}
+          <span style={{ color: '#555' }}>AC:&nbsp;</span>
+          <strong>{monster.ac}</strong>
+        </span>{' '}
+        <span>
+          <span style={{ color: '#555' }}>HP:&nbsp;</span>
+          <strong>{monster.hp}</strong>
+        </span>{' '}
+        <span>
+          <span style={{ color: '#555' }}>XP:&nbsp;</span>
+          <strong>{monster.xp}</strong>
         </span>
-        {/* MID CENTER */}
-        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+      </div>
+      {/* LOW CENTER */}
+      {monster.legendary && (
+        <div>
           <span>
-            <span style={{ color: '#555' }}>AC:&nbsp;</span>
-            <strong>{monster.ac}</strong>
-          </span>{' '}
-          <span>
-            <span style={{ color: '#555' }}>HP:&nbsp;</span>
-            <strong>{monster.hp}</strong>
-          </span>{' '}
-          <span>
-            <span style={{ color: '#555' }}>XP:&nbsp;</span>
-            <strong>{monster.xp}</strong>
+            <span style={{ color: '#555' }}>Legendary Actions:&nbsp;</span>
+            <strong>{monster.legendary}</strong>
           </span>
         </div>
-        {/* LOW CENTER */}
-        {monster.legendary && (
-          <div>
-            <span>
-              <span style={{ color: '#555' }}>Legendary Actions:&nbsp;</span>
-              <strong>{monster.legendary}</strong>
-            </span>
-          </div>
-        )}
-        {monster.lair && (
-          <div>
-            <span>
-              <span style={{ color: '#555' }}>Lair Action Initiative Count:&nbsp;</span>
-              <strong>{monster.lair}</strong>
-            </span>
-          </div>
-        )}
-      </div>
-      {/* RIGHT */}
-      <div style={{ fontSize: '200%', width: 'auto' }}>
-        <strong>
-          {typeof monster.initiative === 'number' ? (
-            initiativeLength(monster.initiative)
-          ) : (
-            <HourglassIcon />
-          )}
-        </strong>
-      </div>
+      )}
+      {monster.lair && (
+        <div>
+          <span>
+            <span style={{ color: '#555' }}>Lair Action Initiative Count:&nbsp;</span>
+            <strong>{monster.lair}</strong>
+          </span>
+        </div>
+      )}
     </div>
-  )
-}
+    {/* RIGHT */}
+    <div style={{ fontSize: '200%', width: 'auto' }}>
+      <strong>
+        {typeof monster.initiative === 'number' ? (
+          initiativeLength(monster.initiative)
+        ) : (
+          <HourglassIcon />
+        )}
+      </strong>
+    </div>
+  </div>
+)
 
 export default MonsterListGroupItem
 
