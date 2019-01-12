d72cdb902f73de6e185bd6836eba566983a50b87
CharmedSatyr <z@charmed.tech>
2019-01-12 05:43:32 -0600

    +comments

--- src/constants/creature.js
+++ src/constants/creature.js
@@ -1,17 +1,17 @@
 class Creature {
   constructor(name, modifier, advantage, id, ac, hp, xp, lair, legendary, tag, number, groupID) {
-    this.name = name
-    this.modifier = parseInt(modifier)
-    this.advantage = advantage
-    this.ac = parseInt(ac)
-    this.hp = parseInt(hp)
-    this.xp = parseInt(xp)
-    this.lair = parseInt(lair)
-    this.legendary = parseInt(legendary)
-    this.tag = tag
-    this.number = parseInt(number)
-    this.id = id
-    this.groupID = groupID || id.split('-')[1]
+    this.name = name // What the creature is called
+    this.modifier = parseInt(modifier) // The creature's initiative modifier
+    this.advantage = advantage // Whether the creature has advantage on initiative rolls
+    this.ac = parseInt(ac) // Armor Class
+    this.hp = parseInt(hp) // Normal Maximum Hit Points
+    this.xp = parseInt(xp) // Total experience gained for killing the creature
+    this.lair = parseInt(lair) // Optional fixed initiative count for lair action
+    this.legendary = parseInt(legendary) // Optional number of legendary actions
+    this.tag = tag // Optional tag label
+    this.number = parseInt(number) // Optional number label
+    this.id = id // Unique identifier per creature, used to delete creatures
+    this.groupID = groupID || id.split('-')[1] // initiative and order are shared by group. groupID used for inc/dec order
   }
 }
 
