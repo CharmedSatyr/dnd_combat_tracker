ca5cd41f3a612c68de51b6d369d0851c3a5bc740
CharmedSatyr <z@charmed.tech>
2019-01-14 18:04:06 -0600

    rendering tests for App component

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,9 +1,26 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import App from '../App'
+import { shallow } from 'enzyme'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
-  ReactDOM.unmountComponentAtNode(div)
+import AddCreature from '../AddCreature/'
+import Initiative from '../Initiative/'
+import Experience from '../Experience/'
+
+describe('`App` component', () => {
+  it('should render without crashing', () => {
+    const app = shallow(<App />)
+    expect(app).toHaveLength(1)
+  })
+  it('should contain one instance of `AddCreature` component', () => {
+    const app = shallow(<App />)
+    expect(app.find(AddCreature)).toHaveLength(1)
+  })
+  it('should contain one instance of `Initiative` component', () => {
+    const app = shallow(<App />)
+    expect(app.find(Initiative)).toHaveLength(1)
+  })
+  it('should contain one instance of `Experience` component', () => {
+    const app = shallow(<App />)
+    expect(app.find(Experience)).toHaveLength(1)
+  })
 })
