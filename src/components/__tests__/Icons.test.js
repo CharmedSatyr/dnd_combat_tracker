2fe098dc055b32d95ca58c3db49802739f176d7c
CharmedSatyr <z@charmed.tech>
2019-01-17 12:01:37 -0600

    basic rendering test for RemoveIcon

---
+++ src/components/__tests__/Icons.test.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import { RemoveIcon } from '../Icons'
+
+describe('`RemoveIcon` component', () => {
+  it('should render without crashing', () => {
+    const props = {
+      creature: {
+        name: 'a',
+        modifier: 1,
+        advantage: false,
+        id: 'Leonidas',
+        groupID: '300',
+      },
+      removeCreature: jest.fn(),
+    }
+    const removeIcon = shallow(<RemoveIcon {...props} />)
+    expect(removeIcon).toHaveLength(1)
+  })
+})
