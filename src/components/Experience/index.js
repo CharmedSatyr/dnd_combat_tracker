cd6734fcab620c697490b87e784b117c636d8a59
CharmedSatyr <z@charmed.tech>
2019-01-17 11:33:49 -0600

    remove need for creatures state

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -4,23 +4,21 @@ import PropTypes from 'prop-types'
 
 import { Col } from 'react-bootstrap'
 
-const Experience = ({ creatures, playerCount, totalXP }) => (
+const Experience = ({ playerCount, totalXP, xpPerPlayer }) => (
   <span>
-    {creatures.length > 0 && (
+    {totalXP > 0 && (
       <Col xs={12} md={4} className="well">
-        <strong>Players:</strong> {parseInt(playerCount)}
+        <strong>Players:</strong> {playerCount}
         <br />
-        <strong>Total Experience:</strong> {parseInt(totalXP)}
+        <strong>Total Experience:</strong> {totalXP}
         <br />
-        <strong>Experience/Player:</strong>{' '}
-        {parseInt(playerCount) > 0 ? Math.floor(parseInt(totalXP) / parseInt(playerCount)) : 0}
+        <strong>Experience/Player:</strong> {xpPerPlayer}
       </Col>
     )}
   </span>
 )
 
 const mapStateToProps = (state, props) => ({
-  creatures: state.creatures,
   playerCount: state.experience.playerCount,
   totalXP: state.experience.totalXP,
   xpPerPlayer: state.experience.xpPerPlayer,
@@ -32,18 +30,6 @@ export default connect(
 )(Experience)
 
 Experience.propTypes = {
-  creatures: PropTypes.arrayOf(
-    PropTypes.shape({
-      name: PropTypes.string.isRequired,
-      modifier: PropTypes.number.isRequired,
-      advantage: PropTypes.bool.isRequired,
-      ac: PropTypes.number,
-      hp: PropTypes.number,
-      xp: PropTypes.number,
-      initiative: PropTypes.number,
-      order: PropTypes.number,
-    })
-  ),
   playerCount: PropTypes.number.isRequired,
   totalXP: PropTypes.number.isRequired,
   xpPerPlayer: PropTypes.number.isRequired,
