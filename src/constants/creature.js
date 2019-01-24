0fe928218a7757a731e21e1cb23254f19fdc03cf
CharmedSatyr <z@charmed.tech>
2019-01-23 21:56:01 -0600

    Alphabetize properties and add derived `type`

--- src/constants/creature.js
+++ src/constants/creature.js
@@ -1,17 +1,18 @@
 class Creature {
   constructor(name, modifier, advantage, id, ac, hp, xp, lair, legendary, tag, number, groupID) {
-    this.name = name // What the creature is called
-    this.modifier = parseInt(modifier) // The creature's initiative modifier
-    this.advantage = advantage // Whether the creature has advantage on initiative rolls
     this.ac = parseInt(ac) // Armor Class
+    this.advantage = advantage // Whether the creature has advantage on initiative rolls
+    this.groupID = groupID || id.split('-')[1] // initiative and order are shared by group. groupID used for inc/dec order
     this.hp = parseInt(hp) // Normal Maximum Hit Points
-    this.xp = parseInt(xp) // Total experience gained for killing the creature
+    this.id = id // Unique identifier per creature, used to delete creatures
     this.lair = parseInt(lair) // Optional fixed initiative count for lair action
     this.legendary = parseInt(legendary) // Optional number of legendary actions
-    this.tag = tag // Optional tag label
+    this.modifier = parseInt(modifier) // The creature's initiative modifier
+    this.name = name // What the creature is called
     this.number = parseInt(number) // Optional number label
-    this.id = id // Unique identifier per creature, used to delete creatures
-    this.groupID = groupID || id.split('-')[1] // initiative and order are shared by group. groupID used for inc/dec order
+    this.tag = tag // Optional tag label
+    this.type = id.split('-')[0] // Monster, Player, Lair Action, etc.
+    this.xp = parseInt(xp) // Total experience gained for killing the creature
   }
 }
 
