c6965c9b7ca7945b4c5cb7ebe1b5a9ccaa108fcb
CharmedSatyr <z@charmed.tech>
2019-01-24 13:28:50 -0600

    Rearrange arguments into alphabetical order

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,17 @@
 import * as c from '../constants/index'
 
-export const addCreatures = creatures => ({ type: c.ADD_CREATURES_TO_STATE, creatures })
+export const addCreatures = creatures => ({ creatures, type: c.ADD_CREATURES_TO_STATE })
 
 export const decrementGroupInitiativeOrder = groupID => ({
-  type: c.DECREMENT_GROUP_INITIATIVE_ORDER,
   groupID,
+  type: c.DECREMENT_GROUP_INITIATIVE_ORDER,
 })
 
 export const incrementGroupInitiativeOrder = groupID => ({
-  type: c.INCREMENT_GROUP_INITIATIVE_ORDER,
   groupID,
+  type: c.INCREMENT_GROUP_INITIATIVE_ORDER,
 })
 
-export const removeCreature = creature => ({ type: c.REMOVE_CREATURE_FROM_STATE, creature })
+export const removeCreature = creature => ({ creature, type: c.REMOVE_CREATURE_FROM_STATE })
 
 export const rollInitiative = () => ({ type: c.ROLL_INITIATIVE })
