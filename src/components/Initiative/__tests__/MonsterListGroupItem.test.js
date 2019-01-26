48abe5d39746a64cbe495d65f34596f7e70ab83f
CharmedSatyr <z@charmed.tech>
2019-01-26 11:32:02 -0600

    Add conditions properties

--- src/components/Initiative/__tests__/MonsterListGroupItem.test.js
+++ src/components/Initiative/__tests__/MonsterListGroupItem.test.js
@@ -8,6 +8,27 @@ describe('`MonsterListGroupItem` component', () => {
       monster: {
         ac: 10,
         advantage: false,
+        conditions: {
+          blinded: false,
+          charmed: false,
+          concentrating: false,
+          custom: [],
+          deafened: false,
+          exhaustion: {
+            level: 0,
+          },
+          frightened: false,
+          grappled: false,
+          incapacitated: false,
+          invisible: false,
+          paralyzed: false,
+          petrified: false,
+          poisoned: false,
+          prone: false,
+          restrained: false,
+          stunned: false,
+          unconscious: false,
+        },
         groupID: 120,
         hp: {
           current: 20,
