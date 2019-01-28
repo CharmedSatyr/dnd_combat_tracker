af0282b7669fd2912052d6a5238a6f30440ce2f4
CharmedSatyr <z@charmed.tech>
2019-01-28 11:56:03 -0600

    Temporarily move Combat up while working on it

--- src/components/App.js
+++ src/components/App.js
@@ -10,11 +10,11 @@ const App = () => (
   <Grid>
     <PageHeader>D&amp;D Initiative &amp; Combat Tracker</PageHeader>
     <Row className="show-grid">
-      <AddCreature />
-      <Initiative />
       <Combat />
     </Row>
     <Row className="show-grid">
+      <AddCreature />
+      <Initiative />
       <Experience />
     </Row>
   </Grid>
