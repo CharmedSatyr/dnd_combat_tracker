1e1c90cb50e2e7d752bd9f4bed35dde67fc06603
Joseph <z@charmed.tech>
2019-02-04 22:56:21 -0600

    Clear bad code and start over

--- src/components/Combat/ConditionsModal.js
+++ src/components/Combat/ConditionsModal.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
-import { Button, ControlLabel, FormGroup, Modal } from 'react-bootstrap'
+import { /* ControlLabel, FormGroup, */ Modal } from 'react-bootstrap'
 import * as a from '../../actions/'
 import Conditions from '../../constants/conditions'
+import ConditionsList from './ConditionsList'
 import { monsterPropTypes } from '../../constants/propTypes'
-import { capitalizeFirstLetter } from '../component.functions'
+// import { capitalizeFirstLetter } from '../component.functions'
 
 class ConditionsModal extends Component {
   constructor(props) {
     super(props)
-    this.state = { conditions: new Conditions(), show: false }
+    this.state = { conditions: new Conditions(), list: [], show: false }
     this.handleClose = this.handleClose.bind(this)
     this.handleShow = this.handleShow.bind(this)
     this.handleToggle = this.handleToggle.bind(this)
@@ -22,58 +23,22 @@ class ConditionsModal extends Component {
     this.setState({ show: true })
   }
   handleToggle(val) {
-    if (val !== 'custom' && val !== 'exhaustion') {
-      this.props.toggleCondition(this.props.monster, val)
-    }
+    this.props.toggleCondition(this.props.monster, val)
   }
   render() {
-    const conditions = new Conditions()
-    const list = []
-    for (let val in conditions) {
-      const { level } = conditions.exhaustion
-      if (val === 'custom') {
-        const subList = []
-        conditions[val].forEach(c => subList.push(<li key={c}>{c}</li>))
-        list.push(
-          <li key={val}>
-            Custom Conditions:
-            <br />
-            {subList}
-          </li>
-        )
-      } else if (val === 'exhaustion') {
-        list.push(<li key={level}>Exhaustion (Level {level})</li>)
-      } else {
-        list.push(
-          <li key={val}>
-            {
-              <FormGroup validationState={null}>
-                <ControlLabel>{capitalizeFirstLetter(val)}</ControlLabel>
-                <input
-                  onChange={this.handleToggle(val)}
-                  id={val}
-                  type="checkbox"
-                  checked={conditions[val]}
-                />
-              </FormGroup>
-            }
-          </li>
-        )
-      }
-    }
-
     return (
       <div>
-        <Button bsStyle="primary" bsSize="small" onClick={this.handleShow}>
-          Update Conditions
-        </Button>
+        <div style={{ height: '100%', width: '100%' }} onClick={this.handleShow}>
+          <ConditionsList monster={this.props.monster} />
+          &nbsp;
+        </div>
         <Modal show={this.state.show} onHide={this.handleClose}>
           <Modal.Header closeButton>
             <Modal.Title>Update Conditions</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <h4>This is a thing!</h4>
-            <ul>{list}</ul>
+            <h4>Conditions:</h4>
+            <ul>{}</ul>
           </Modal.Body>
         </Modal>
       </div>
