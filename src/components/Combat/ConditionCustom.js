843411e64709a374779c6b20ab08081fbeb8740d
Joseph <z@charmed.tech>
2019-02-05 17:58:53 -0600

    Add ability to add custom condition

---
+++ src/components/Combat/ConditionCustom.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import * as a from '../../actions/'
+import { Button, Glyphicon, FormControl, InputGroup, ListGroupItem } from 'react-bootstrap'
+
+class ConditionCustom extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { custom: 'No custom conditions.', input: '' }
+    this.handleAdd = this.handleAdd.bind(this)
+    this.handleInput = this.handleInput.bind(this)
+    this.handleRemove = this.handleRemove.bind(this)
+  }
+  handleAdd() {
+    this.props.addCustomCondition(this.props.monster, this.state.input)
+    this.setState({ input: '' })
+    this.updateConditions()
+  }
+  handleRemove(condition) {
+    this.props.removeCustomCondition(this.props.monster, condition)
+    this.updateConditions()
+  }
+  handleInput(e) {
+    this.setState({ input: e.target.value })
+  }
+  updateConditions() {
+    const custom = this.props.monster.conditions.custom.map(c => {
+      return (
+        <Button bsSize="xsmall" key={c} onClick={() => this.handleRemove(c)}>
+          <Glyphicon glyph="remove" style={{ color: '#a94442' }} title="Remove custom condition" />
+          &nbsp;
+          {c}
+        </Button>
+      )
+    })
+    if (custom.length > 0) {
+      this.setState({ custom })
+    } else {
+      this.setState({ custom: 'No custom conditions.' })
+    }
+  }
+  componentWillMount() {
+    this.updateConditions()
+  }
+  render() {
+    return (
+      <ListGroupItem>
+        <div style={{ alignItems: 'center', display: 'flex', justifyContent: 'space-between' }}>
+          {/* Label */}
+          <div>Custom Conditions</div>
+          {/* Input to add new custom condition */}
+          <div style={{ width: '32%' }}>
+            <InputGroup>
+              <FormControl
+                onChange={this.handleInput}
+                placeholder="On fire!"
+                type="text"
+                value={this.state.input}
+              />
+              <InputGroup.Button>
+                <Button onClick={this.handleAdd}>Submit</Button>
+              </InputGroup.Button>
+            </InputGroup>
+          </div>
+          {/* List of custom conditions */}
+          <div
+            style={{
+              alignItems: 'center',
+              display: 'flex',
+              justifyContent: 'flex-end',
+              flexWrap: 'wrap',
+              width: '32%',
+            }}
+          >
+            {this.state.custom}
+          </div>
+        </div>
+      </ListGroupItem>
+    )
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  addCustomCondition: (creature, condition) => dispatch(a.addCustomCondition(creature, condition)),
+  removeCustomCondition: (creature, condition) =>
+    dispatch(a.removeCustomCondition(creature, condition)),
+})
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(ConditionCustom)
