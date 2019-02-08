2330bd6741b12c5cc541d64535054f93a4adc1d1
Joseph <z@charmed.tech>
2019-02-08 13:11:06 -0600

    Totally revamp styling, include all info

--- src/components/Combat/CombatListGroupItem.js
+++ src/components/Combat/CombatListGroupItem.js
@@ -6,30 +6,34 @@ import DamageHealForm from './DamageHealForm'
 import { MonsterIcon } from '../Icons'
 import ConditionsModal from './ConditionsModal'
 
+import { initiativeLength } from '../component.functions'
+
 const MonsterStats = ({ monster }) => (
   <div>
     <MonsterIcon />
     <strong>{monster.name}</strong>&nbsp;<Label>{setLabel(monster.tag, monster.number)}</Label>
     <br />
-    <span style={{ color: '#555' }}>Modifier:&nbsp;</span>
-    <strong>{monster.modifier >= 0 ? `+${monster.modifier}` : `${monster.modifier}`}</strong>
-    <br />
-    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-      <span>
-        <span style={{ color: '#555' }}>AC:&nbsp;</span>
-        <strong>{monster.ac}</strong>
-      </span>
-      <span>
-        <span style={{ color: '#555' }}>XP:&nbsp;</span>
-        <strong>{monster.xp}</strong>
-      </span>
-    </div>
+    <span style={{ fontSize: 11 }}>
+      <span style={{ color: '#555' }}>Initiative Modifier:&nbsp;</span>
+      <strong>{monster.modifier >= 0 ? `+${monster.modifier}` : `${monster.modifier}`}</strong>
+      <br />
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <span>
+          <span style={{ color: '#555' }}>AC:&nbsp;</span>
+          <strong>{monster.ac}</strong>
+        </span>
+        <span>
+          <span style={{ color: '#555' }}>XP:&nbsp;</span>
+          <strong>{monster.xp}</strong>
+        </span>
+      </div>
+    </span>
   </div>
 )
 
 const HitPoints = ({ hp }) => (
-  <div>
-    <div style={{ color: '#555', fontVariant: 'small-caps', textAlign: 'center' }}>hit points</div>
+  <div style={{ height: '100%' }}>
+    <div style={{ color: '#555', fontVariant: 'small-caps' }}>hit points</div>
     <span style={{ fontSize: '175%' }}>
       <strong>
         {hp.current}/{hp.max}
@@ -38,7 +42,7 @@ const HitPoints = ({ hp }) => (
   </div>
 )
 
-const CombatListGroupItem = ({ monster }) => (
+const CombatListGroupItem = ({ monster, removeCreature }) => (
   <ListGroupItem
     className="list-group-item"
     style={{
@@ -50,59 +54,96 @@ const CombatListGroupItem = ({ monster }) => (
     {/* LEFT */}
     <div
       style={{
-        border: '0.5px solid #555',
+        border: '1px solid #ddd',
         fontVariant: 'small-caps',
         gridRow: '1 / span 2',
       }}
     >
       order
+      <div
+        style={{
+          alignItems: 'center',
+          display: 'flex',
+          flexDirection: 'column',
+          fontSize: '200%',
+          height: '80%',
+          justifyContent: 'center',
+        }}
+      >
+        <strong>{monster.order}</strong>
+      </div>
     </div>
     {/* TOP LEFT */}
-    <div style={{ border: '0.5px solid #555', placeSelf: 'stretch stretch' }}>
+    <div
+      style={{
+        borderRight: '1px solid #ddd',
+        borderTop: '1px solid #ddd',
+        placeSelf: 'stretch stretch',
+      }}
+    >
       <MonsterStats monster={monster} />
     </div>
     {/* TOP CENTER */}
     <div
       style={{
-        border: '0.5px solid #555',
+        borderTop: '1px solid #ddd',
         placeSelf: 'stretch center',
       }}
     >
       <DamageHealForm monster={monster} />
     </div>
     {/* TOP RIGHT */}
-    <div style={{ border: '0.5px solid #555', placeSelf: 'stretch center' }}>
+    <div
+      style={{
+        borderLeft: '1px solid #ddd',
+        borderTop: '1px solid #ddd',
+        placeSelf: 'stretch center',
+      }}
+    >
       <HitPoints hp={monster.hp} />
     </div>
     {/* RIGHT */}
     <div
       style={{
-        border: '0.5px solid #555',
+        border: '1px solid #ddd',
         gridColumn: '5',
         gridRow: '1 / span 2',
         placeSelf: 'stretch stretch',
       }}
     >
       <span style={{ fontVariant: 'small-caps' }}>initiative</span>
+      <div
+        style={{
+          alignItems: 'center',
+          display: 'flex',
+          fontSize: '200%',
+          height: '80%',
+          justifyContent: 'center',
+          width: 'auto',
+        }}
+      >
+        <strong>{monster.initiative && initiativeLength(monster.initiative)}</strong>
+      </div>
     </div>
     {/* BOTTOM LEFT */}
     <div
       style={{
-        border: '0.5px solid #555',
+        borderBottom: '1px solid #ddd',
+        borderTop: '1px solid #ddd',
         placeSelf: 'stretch stretch',
       }}
     >
       <span style={{ color: '#555', fontVariant: 'small-caps' }}>special</span>
       {monster.lair && (
-        <div>
+        <div style={{ fontSize: 11 }}>
           <span>
-            <span style={{ color: '#555' }}>Lair Action Initiative Count:&nbsp;</span>
+            <span style={{ color: '#555' }}>Lair Action Initiative:&nbsp;</span>
             <strong>{monster.lair}</strong>
           </span>
         </div>
       )}
       {monster.legendary && (
-        <div>
+        <div style={{ fontSize: 11 }}>
           <span>
             <span style={{ color: '#555' }}>Legendary Actions:&nbsp;</span>
             <strong>{monster.legendary}</strong>
@@ -113,7 +154,9 @@ const CombatListGroupItem = ({ monster }) => (
     {/* BOTTOM CENTER/RIGHT */}
     <div
       style={{
-        border: '0.5px solid #555',
+        borderBottom: '1px solid #ddd',
+        borderLeft: '1px solid #ddd',
+        borderTop: '1px solid #ddd',
         placeSelf: 'stretch stretch',
         gridColumn: '3 / span 2',
       }}
