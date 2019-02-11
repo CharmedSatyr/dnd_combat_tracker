4558f15622ee6ae0602c18a861a571e931b4a072
Joseph <z@charmed.tech>
2019-02-11 16:31:40 -0600

    Convert to functional component

--- src/components/Combat/index.js
+++ src/components/Combat/index.js
@@ -1,33 +1,19 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React from 'react'
 import { ListGroup } from 'react-bootstrap'
-import { creaturesPropTypes } from '../../constants/propTypes'
 import CombatListGroupItem from './CombatListGroupItem'
 
-class Combat extends Component {
-  render() {
-    const { creatures } = this.props
-    let creatureList
-    if (creatures) {
-      creatureList = creatures.map((c, i) => {
-        if (c.type === 'monster') {
-          return <CombatListGroupItem key={i} monster={c} />
-        } else {
-          return null
-        }
-      })
-    }
-    return (
-      creatureList.length > 0 && <div>{creatureList && <ListGroup>{creatureList}</ListGroup>}</div>
-    )
+const Combat = ({ creatures }) => {
+  let creatureList
+  if (creatures) {
+    creatureList = creatures.map((c, i) => {
+      if (c.type === 'monster') {
+        return <CombatListGroupItem key={i} monster={c} />
+      } else {
+        return null
+      }
+    })
   }
+  return <ListGroup>{creatureList}</ListGroup>
 }
 
-const mapStateToProps = state => ({ creatures: state.creatures })
-
-export default connect(
-  mapStateToProps,
-  null
-)(Combat)
-
-Combat.propTypes = { ...creaturesPropTypes }
+export default Combat
