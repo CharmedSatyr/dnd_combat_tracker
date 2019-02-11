74865afab626bbea2de1eb8dc1939ea3d6b3a443
Joseph <z@charmed.tech>
2019-02-11 16:33:05 -0600

    Save updates to localStorage; syntax tweaks

--- src/components/Initiative/index.js
+++ src/components/Initiative/index.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import PropTypes from 'prop-types'
-import { creaturesPropTypes } from '../../constants/propTypes'
-
 import { ListGroup } from 'react-bootstrap'
+import PropTypes from 'prop-types'
 
 import * as a from '../../actions'
 
 import {
-  findCreaturesInLocalStorage,
   removeCreatureFromLocalStorage,
+  updateCreaturesInLocalStorage,
 } from '../localStorage.functions'
 
 import LairActionListGroupItem from './LairActionListGroupItem'
@@ -19,19 +17,22 @@ import RemoveButtons from './RemoveButtons'
 import RollButton from './RollButton'
 
 class Initiative extends Component {
-  componentDidMount() {
-    findCreaturesInLocalStorage(this.props.addCreatures)
+  constructor(props) {
+    super(props)
+    this.roll = this.roll.bind(this)
+  }
+  async roll() {
+    await this.props.rollInitiative()
+    updateCreaturesInLocalStorage(this.props.creatures)
   }
   render() {
-    const { creatures, removeCreature, rollInitiative } = this.props
+    const { creatures, removeCreature } = this.props
     let creatureList
     if (creatures) {
       creatureList = creatures.map((c, i) => {
-        if (c.type === 'lair-action') {
-          return <LairActionListGroupItem key={i} monster={c} />
-        }
-        /* Not using `c.id` for key to avoid problems when saved, static groups are added */
         switch (c.type) {
+          case 'lair-action':
+            return <LairActionListGroupItem key={i} monster={c} />
           case 'player':
             return <PlayerListGroupItem key={i} player={c} removeCreature={removeCreature} />
           case 'monster':
@@ -41,26 +42,20 @@ class Initiative extends Component {
         }
       })
     }
-
     return (
-      creatureList.length > 0 && (
-        <div>
-          {creatureList && <ListGroup>{creatureList}</ListGroup>}
-
-          <RollButton rollFunction={rollInitiative} />
-          <hr />
-          <RemoveButtons
-            creatures={creatures}
-            removeCreature={removeCreature}
-            removeLocal={removeCreatureFromLocalStorage}
-          />
-        </div>
-      )
+      <div>
+        <ListGroup>{creatureList}</ListGroup>
+        <RollButton rollFunction={this.roll} />
+        <hr />
+        <RemoveButtons
+          creatures={creatures}
+          removeCreature={removeCreature}
+          removeLocal={removeCreatureFromLocalStorage}
+        />
+      </div>
     )
   }
 }
-
-const mapStateToProps = state => ({ creatures: state.creatures })
 
 const mapDispatchToProps = dispatch => ({
   addCreatures: localCreatures => dispatch(a.addCreatures(localCreatures)),
@@ -69,13 +64,12 @@ const mapDispatchToProps = dispatch => ({
 })
 
 export default connect(
-  mapStateToProps,
+  null,
   mapDispatchToProps
 )(Initiative)
 
 Initiative.propTypes = {
   addCreatures: PropTypes.func.isRequired,
-  ...creaturesPropTypes,
   removeCreature: PropTypes.func.isRequired,
   rollInitiative: PropTypes.func.isRequired,
 }
