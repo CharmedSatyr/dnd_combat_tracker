d158b168479ac3be0e71e9ac5610e77771831d07
Joseph <z@charmed.tech>
2019-02-12 16:10:26 -0600

    Return a ListGroupItem with message if no monsters

--- src/components/Combat/index.js
+++ src/components/Combat/index.js
@@ -1,19 +1,31 @@
 import React from 'react'
-import { ListGroup } from 'react-bootstrap'
+import { ListGroup, ListGroupItem } from 'react-bootstrap'
 import CombatListGroupItem from './CombatListGroupItem'
 
 const Combat = ({ creatures }) => {
   let creatureList
   if (creatures) {
-    creatureList = creatures.map((c, i) => {
-      if (c.type === 'monster') {
-        return <CombatListGroupItem key={i} monster={c} />
-      } else {
-        return null
-      }
-    })
+    creatureList = creatures
+      .map((c, i) => {
+        if (c.type === 'monster') {
+          return <CombatListGroupItem key={i} monster={c} />
+        } else {
+          return null
+        }
+      })
+      .filter(c => c)
   }
-  return <ListGroup>{creatureList}</ListGroup>
+  return (
+    <ListGroup>
+      {creatureList.length > 0 ? (
+        creatureList
+      ) : (
+        <ListGroupItem className="list-group-item">
+          <strong>No monsters have been added.</strong>
+        </ListGroupItem>
+      )}
+    </ListGroup>
+  )
 }
 
 export default Combat
